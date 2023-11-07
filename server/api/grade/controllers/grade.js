631af5b49a0a43ce3127ab6195a6508e89ac2cc9
Logan Chenicek <ltcollege401@gmail.com>
2023-11-06 20:12:56 -0500

    Removing some out of date code

--- server/api/grade/controllers/grade.js
+++ server/api/grade/controllers/grade.js
@@ -5,39 +5,4 @@
  * to customize this controller
  */
 
-module.exports = {
-    async find(ctx) {
-        /*
-        const {students, classroom} = ctx.request.body
-
-        if (students) {
-            return await Promise.all(students.map(student => {
-                // validate the request
-                if (typeof student.name !== "string" || !student.name) return ctx.badRequest(
-                    'A name must be provided!',
-                    {id: 'Student.create.body.invalid', error: 'ValidationError'}
-                )
-
-                return strapi.services.student.create({
-                    name: student.name,
-                    character: student.character,
-                    classroom: classroom
-                })
-            }))
-        }
-
-        // validate the request
-        const {name} = ctx.request.body
-        if (typeof name !== "string" || !name) return ctx.badRequest(
-            'A name must be provided!',
-            {id: 'Student.create.body.invalid', error: 'ValidationError'}
-        )
-
-        return strapi.services.student.create(ctx.request.body)
-        */
-        return ctx.badRequest(
-            'Seeing if my change is reflected',
-            {id: 'Grade.find.body.invalid', error: 'ValidationError'}
-        )
-    }
-};
+module.exports = {};
