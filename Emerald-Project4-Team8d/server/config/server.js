70865b446842d7c55fe7dabed2edb11f541e47ea
RidaShaik <ridaseenshaik@gmail.com>
2023-11-27 22:50:56 -0500

    minor tweaks

---
+++ Emerald-Project4-Team8d/server/config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '7ea40fd10ed0137630d4b5942ca52a5c'),
+    },
+  },
+});
