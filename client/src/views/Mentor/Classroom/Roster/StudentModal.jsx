bc33da139a38294e578fd659bd6286380939efb0
RidaShaik <ridaseenshaik@gmail.com>
2023-12-05 16:45:50 -0500

    Teacher view of student submission and commented code

--- client/src/views/Mentor/Classroom/Roster/StudentModal.jsx
+++ client/src/views/Mentor/Classroom/Roster/StudentModal.jsx
@@ -7,7 +7,12 @@ import './Roster.less';
 
 export default function StudentModal({ linkBtn, student, getFormattedDate }) {
     const [visible, setVisible] = useState(false);
-    const [submissions, setSubmissions] = useState([]);
+
+    // MY CODE BELOW
+
+    const [submissions, setSubmissions] = useState([]); //Handles the submissions used in studentSubmissions component
+
+    //MY CODE ABOVE
 
     const showModal = () => {
         setVisible(true);
@@ -22,11 +27,13 @@ export default function StudentModal({ linkBtn, student, getFormattedDate }) {
     };
 
 
+    //MY CODE BELOW
+
     const studentSubmission = async (id) => {  //Asynchronous arrow function that handles the student submissions from strappi admin
         const studentResponse = await getStudent(id); //retrieves student information via student id
         const newSubmissions = await Promise.all(
             studentResponse.data.sessions.map(async (session) => {
-                const studNames = await Promise.all(  //Fethces student session data and stores into studNames, essentially adds activity names
+                const studNames = await Promise.all(  //Fetches student session data and stores into studNames, essentially adds activity names
                     (await getSession(session.id)).data.submissions.map(async (submission) => ({
                         ...submission, //Creates a new object
                         name: (await getActivity(submission.activity)).data.lesson_module.name, //Adds property name to the submission object
@@ -40,6 +47,8 @@ export default function StudentModal({ linkBtn, student, getFormattedDate }) {
         console.log(submissions); //logs submission values into console to be used in the app
     };
 
+    //MY CODE ABOVE
+
 
     return (
         <div>
@@ -52,12 +61,15 @@ export default function StudentModal({ linkBtn, student, getFormattedDate }) {
                 onCancel={handleCancel}
                 footer={[
 
+
+                    //MY CODE BELOW
+
                     <Button key='submissions' type='secondary' onClick={( //Create a button for submissions under view in roster
-                        studentSubmission(student.enrolled.id)
+                        studentSubmission(student.enrolled.id) //Handles button
                     )}
-                            style={{ marginBottom: '10px' }}
-                    >
-                        Submissions
+                            //Styling
+                            style={{ marginBottom: '10px' }} //Below is the name for the submission button
+                    > Submissions
                     </Button>,
 
                     <Button key='ok' type='primary' onClick={handleOk}>
@@ -67,15 +79,16 @@ export default function StudentModal({ linkBtn, student, getFormattedDate }) {
                     <div>
                         {submissions && submissions.length > 0 ? ( //Checks variable exists
                             submissions.map((submission, index) => ( //Checks submission array and creates element for each submission
-                                //below shows submission name
-                                //HERE
+                                //Styling to make the submissions pop up look more pleasing and organized
+                                //Under the submission button contains submissions
+                                //Below is the Submission name followed but when submitted
                                 <div style={{ textAlign: 'left' }}>
                                     <div style={{ display: 'flex', justifyContent: 'space-between', alignItems: 'center' }}>
                                         <div>
                                             Submission for "{submission.name}"
                                             {submission.updated_at}
                                         </div>
-                                        <Button key='code' type='primary'
+                                        <Button key='code' type='primary' //Code button used to get a pop up that shows the code submitted by student
                                                 onClick={() => {
                                                     handleOk(); // Close the modal if needed
                                                 }}
@@ -84,11 +97,14 @@ export default function StudentModal({ linkBtn, student, getFormattedDate }) {
                                         </Button>
                                     </div>
                                 </div>
-                            ))
+                            )) //Above is the name for the code button
                         ) : (
                             <div className="pain">No Submissions</div> //If submission array is empty display no submission
                         )}
                     </div>
+
+                    //MY CODE ABOVE
+
 
                 ]}
             >
